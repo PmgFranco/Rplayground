e54c8038eb4e68c75cdb4f5b553e66d7ab971dff
pmfranco <pmfranco@kpmg.com>
2016-07-26 14:26:43 +0100

    1st commit

---
+++ R/splitSets.R
@@ -0,0 +1,8 @@
+splitSets = function(dataframe, seed=NULL) {
+  if (!is.null(seed)) set.seed(seed)
+  index <- 1:nrow(dataframe)
+  trainindex <- sample(index, trunc(length(index)/2))
+  trainset <- dataframe[trainindex, ]
+  testset <- dataframe[-trainindex, ]
+  list(trainset=trainset,testset=testset)
+}
